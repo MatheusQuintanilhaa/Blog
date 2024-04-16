7ac3c552e4cd88e4c44e308c0b18be54c42bc5ec
Matheus Quintanilha <matheus@matheus.com.br>
2024-04-16 11:18:24 -0300

    Styling the menu.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,36 @@
+import logosvg from "../../svg/logo.svg";
+
 const Header = () => {
-  return <h1>Header.js</h1>;
+  const descLogo = "Essa é a logo do Blog.";
+  return (
+    <>
+      <header className="flex-space-between">
+        <div className="logo border">
+          <img src={logosvg} alt={descLogo} />
+        </div>
+        <div className="search">
+          <input type="text" className="input-search" name="search" />
+        </div>
+        <ul className="menu">
+          <li>
+            <a href="#" className="nav-link">
+              Categories
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
 };
 
 export default Header;
